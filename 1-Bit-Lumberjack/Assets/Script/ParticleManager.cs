2d9c7592edb93b4c2dd521de39d134e05dadf2dd
Po2s <pondandmeen@gmail.com>
2024-04-03 17:29:55 +0700

    particle system and tab particle

--- 1-Bit-Lumberjack/Assets/Script/ParticleManager.cs
+++ 1-Bit-Lumberjack/Assets/Script/ParticleManager.cs
@@ -1,8 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ParticleManager : Singleton<ParticleManager>
 {
+    public Particle[] particles;
 
+    public void SpawnParticle(string name, Vector3 pos)
+    {
+        Particle p = Array.Find(particles, p => p.name == name);
+        if (p == null) return;
+        Vector3 worldPos = new Vector3(pos.x, pos.y, 0f);
+        GameObject pObj = Instantiate(p.prefab, worldPos, Quaternion.identity);
+        Animator a = pObj.GetComponent<Animator>();
+        Destroy(pObj, a.GetCurrentAnimatorStateInfo(0).length);
+    }
+
+}
+
+[Serializable]
+public class Particle
+{
+    public string name;
+    public GameObject prefab;
 }
--- 1-Bit-Lumberjack/Assets/Script/Player/PlayerManager.cs
+++ 1-Bit-Lumberjack/Assets/Script/Player/PlayerManager.cs
@@ -5,13 +5,25 @@ using UnityEngine.EventSystems;
 
 public class PlayerManager : Singleton<PlayerManager>
 {
-    public LayerMask UIMask;
+    //Ref
+    [HideInInspector] public Animator anim;
+
+    // Input
     [HideInInspector] public Vector2 touchPoint;
+
+    //Variable
+
 
     private void OnEnable()
     {
         InputSystem.OnTapEvent += Tap;
     }
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
 
     private void OnDisable()
     {
@@ -20,12 +32,20 @@ public class PlayerManager : Singleton<PlayerManager>
 
     public void Tap()
     {
+        ParticleManager.Instance.SpawnParticle("TapParticle", GetWorldPosFormTouchPoint3D());
         Attack();
     }
 
     public void Attack()
     {
         Debug.Log("Attack");
+        anim.Play("Player_Attack");
+    }
+
+    public Vector3 GetWorldPosFormTouchPoint3D()
+    {
+        Vector3 tapWorldPos = Camera.main.ScreenToWorldPoint(touchPoint);
+        return tapWorldPos;
     }
 
 }
