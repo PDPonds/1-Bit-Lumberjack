9f7a789fa0c109507d1d6c64ad7d78e6068f74c5
Po2s <pondandmeen@gmail.com>
2024-04-05 02:42:59 +0700

    show menu border

--- 1-Bit-Lumberjack/Assets/Script/UIManager.cs
+++ 1-Bit-Lumberjack/Assets/Script/UIManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 
 public class UIManager : Singleton<UIManager>
 {
+    delegate void OnFuncFinsh();
+
     [Header("===== Coin =====")]
     public Transform coinIcon;
     [SerializeField] TextMeshProUGUI coinText;
@@ -16,6 +18,14 @@ public class UIManager : Singleton<UIManager>
     [SerializeField] TextMeshProUGUI phaseText;
     [Header("===== State =====")]
     [SerializeField] TextMeshProUGUI stateText;
+    [Header("===== Menu =====")]
+    [SerializeField] Button axeButton;
+    [SerializeField] GameObject axeBorder;
+    [SerializeField] Button teamButton;
+    [SerializeField] GameObject teamBorder;
+    [SerializeField] Button achievementButton;
+    [SerializeField] GameObject achievementBorder;
+    [SerializeField] Button[] closeMenus;
 
     private void OnEnable()
     {
@@ -25,6 +35,17 @@ public class UIManager : Singleton<UIManager>
         GameManager.Instance.OnNextPhase += UpdatePhase;
         EnemyController.Instance.OnEnemyTakeDamage += UpdateHP;
         EnemyController.Instance.OnEnemyDead += UpdateHP;
+    }
+
+    private void Awake()
+    {
+        axeButton.onClick.AddListener(() => Show(axeBorder));
+        teamButton.onClick.AddListener(() => Show(teamBorder));
+        achievementButton.onClick.AddListener(() => Show(achievementBorder));
+        foreach (Button b in closeMenus)
+        {
+            b.onClick.AddListener(() => CloseMenu());
+        }
     }
 
     private void Start()
@@ -59,6 +80,45 @@ public class UIManager : Singleton<UIManager>
         float percent = (float)cur / (float)max;
         hpFill.fillAmount = percent;
         hpText.text = cur.ToString();
+    }
+
+    void Move(GameObject go, Vector3 pos, float time)
+    {
+        LeanTween.move(go.GetComponent<RectTransform>(), pos, time)
+            .setEaseInOutCubic();
+    }
+
+    void Move(GameObject go, Vector3 pos, float time, OnFuncFinsh finish)
+    {
+        LeanTween.move(go.GetComponent<RectTransform>(), pos, time)
+            .setEaseInOutCubic()
+            .setOnComplete(() => finish?.Invoke());
+    }
+
+    void Show(GameObject go)
+    {
+        CloseMenuAll();
+        go.SetActive(true);
+        Move(axeBorder, new Vector3(0, 0, 0), 0.5f);
+        Move(teamBorder, new Vector3(0, 0, 0), 0.5f);
+        Move(achievementBorder, new Vector3(0, 0, 0), 0.5f);
+    }
+
+    void CloseMenu()
+    {
+        Move(axeBorder, new Vector3(0, -450, 0), 0.5f,
+            () => axeBorder.SetActive(false));
+        Move(teamBorder, new Vector3(0, -450, 0), 0.5f,
+            () => teamBorder.SetActive(false));
+        Move(achievementBorder, new Vector3(0, -450, 0), 0.5f,
+            () => achievementBorder.SetActive(false));
+    }
+
+    void CloseMenuAll()
+    {
+        axeBorder.SetActive(false);
+        teamBorder.SetActive(false);
+        achievementBorder.SetActive(false);
     }
 
 }
